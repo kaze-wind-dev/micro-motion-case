d045010338e21ca4dea2fba26efd7804661c84f3
HayateIwamoto <iwamoto.hayate.613@gmail.com>
2025-06-22 21:28:13 +0900

    add: お知らせの追加

--- app/page.tsx
+++ app/page.tsx
@@ -1,103 +1,94 @@
 import Image from "next/image";
+import React from "react";
 
+import { getArticleList } from "@/libs/microcmsFunctions";
+import type { News } from "@/types";
+
+// APIから記事一覧を取得
+const news = await getArticleList({
+  endpoint: "news", // 取得したいエンドポイント名を指定
+  queries: { 
+    limit: 3,
+   }, // オプションで取得件数やオフセットを指定可能
+});
+// console.log(data);
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+    <div className="">
+      <main className="">
+        <h1 className="font-base text-[5rem]">
+          何かテキストを入れてくれNotosans
+        </h1>
+        <h1 className="font-mono text-[5rem] ">MonoFont</h1>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
+        <section className="py-16 md:py-24 lg:py-32">
+          <div className="inner">
+            <hgroup className="title mb-10 md:mb-16 lg:mb-20">
+              <h2>お知らせ</h2>
+              <p>News</p>
+            </hgroup>
+            <p className="text-base text-center leading-loose">
+              最新情報をお届けします。
+            </p>
+            {news.contents.length === 0 ? (
+              <p>投稿がありません</p>
+            ) : (
+              <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 md:gap-6 lg:gap-8 mt-10">
+                {news.contents.map((article: News) => {
+                  return (
+                    <article key={article.id}>
+                      <a
+                        href={`/news/${article.id}`}
+                        className="py-6 lg:py-8 px-5 bg-white rounded-xl shadow-lg mb-4 hover:bg-green-50 hover:shadow-sm transition-all duration-300 block"
+                      >
+                        <header className="mb-2 md:mb-3 lg:mb-4">
+                          <p className="text-xs text-right text-gray-500 mb-2 lg:mb-3">
+                            <time>
+                              {article.publishedAt
+                                ? article.publishedAt
+                                : article.createdAt}
+                            </time>
+                          </p>
+                          <figure className="mb-4 aspect-[3/2]">
+                            {article.thumbnail ? (
+                              <Image
+                                src={article.thumbnail.url}
+                                width={article.thumbnail.width}
+                                height={article.thumbnail.height}
+                                alt={
+                                  article.thumbnail.alt
+                                    ? article.thumbnail.alt
+                                    : ""
+                                }
+                                className="w-full h-full object-cover rounded-lg"
+                              />
+                            ) : (
+                              <Image
+                                src="/images/no-img.jpg"
+                                width={600}
+                                height={400}
+                                alt="no image"
+                                className="w-full h-full object-cover rounded-lg"
+                              />
+                            )}
+                          </figure>
+                        </header>
+                        <h3 className="text-md font-bold mb-2 line-clamp-1">
+                          {article.title}
+                        </h3>
+                        <p className="text-sm line-clamp-2">
+                          {article.description}
+                        </p>
+      
+                      </a>
+                    </article>
+                  );
+                })}
+              </div>
+            )}
+          </div>
+        </section>
       </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
     </div>
   );
 }
