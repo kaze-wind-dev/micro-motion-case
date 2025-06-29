9f9502acdfbc2d1752c37ab1d4a9d312014b0f6b
HayateIwamoto <iwamoto.hayate.613@gmail.com>
2025-06-29 18:06:29 +0900

    add: AnimationSectionの追加

--- app/page.tsx
+++ app/page.tsx
@@ -1,94 +1,36 @@
-import Image from "next/image";
-import React from "react";
 
+// import Image from "next/image";
 import { getArticleList } from "@/libs/microcmsFunctions";
-import type { News } from "@/types";
+import NewsCardList from "@/components/NewsCardList";
+import Hero from "@/components/Hero";
+import AnimationSection from "@/components/AnimationSection";
 
 // APIから記事一覧を取得
 const news = await getArticleList({
   endpoint: "news", // 取得したいエンドポイント名を指定
-  queries: { 
+  queries: {
     limit: 3,
-   }, // オプションで取得件数やオフセットを指定可能
+  },
 });
-// console.log(data);
-export default function Home() {
-  return (
-    <div className="">
-      <main className="">
-        <h1 className="font-base text-[5rem]">
-          何かテキストを入れてくれNotosans
-        </h1>
-        <h1 className="font-mono text-[5rem] ">MonoFont</h1>
 
-        <section className="py-16 md:py-24 lg:py-32">
-          <div className="inner">
-            <hgroup className="title mb-10 md:mb-16 lg:mb-20">
-              <h2>お知らせ</h2>
-              <p>News</p>
-            </hgroup>
-            <p className="text-base text-center leading-loose">
-              最新情報をお届けします。
-            </p>
-            {news.contents.length === 0 ? (
-              <p>投稿がありません</p>
-            ) : (
-              <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 md:gap-6 lg:gap-8 mt-10">
-                {news.contents.map((article: News) => {
-                  return (
-                    <article key={article.id}>
-                      <a
-                        href={`/news/${article.id}`}
-                        className="py-6 lg:py-8 px-5 bg-white rounded-xl shadow-lg mb-4 hover:bg-green-50 hover:shadow-sm transition-all duration-300 block"
-                      >
-                        <header className="mb-2 md:mb-3 lg:mb-4">
-                          <p className="text-xs text-right text-gray-500 mb-2 lg:mb-3">
-                            <time>
-                              {article.publishedAt
-                                ? article.publishedAt
-                                : article.createdAt}
-                            </time>
-                          </p>
-                          <figure className="mb-4 aspect-[3/2]">
-                            {article.thumbnail ? (
-                              <Image
-                                src={article.thumbnail.url}
-                                width={article.thumbnail.width}
-                                height={article.thumbnail.height}
-                                alt={
-                                  article.thumbnail.alt
-                                    ? article.thumbnail.alt
-                                    : ""
-                                }
-                                className="w-full h-full object-cover rounded-lg"
-                              />
-                            ) : (
-                              <Image
-                                src="/images/no-img.jpg"
-                                width={600}
-                                height={400}
-                                alt="no image"
-                                className="w-full h-full object-cover rounded-lg"
-                              />
-                            )}
-                          </figure>
-                        </header>
-                        <h3 className="text-md font-bold mb-2 line-clamp-1">
-                          {article.title}
-                        </h3>
-                        <p className="text-sm line-clamp-2">
-                          {article.description}
-                        </p>
-      
-                      </a>
-                    </article>
-                  );
-                })}
-              </div>
-            )}
-          </div>
-        </section>
-      </main>
-    </div>
+export default function Home() {
+
+  return (
+    <main className="">
+      <Hero />
+      <AnimationSection/>
+      <section className="py-16 md:py-24 lg:py-32">
+        <div className="inner">
+          <hgroup className="title mb-10 md:mb-16 lg:mb-20">
+            <h2>お知らせ</h2>
+            <p>News</p>
+          </hgroup>
+          <p className="text-base text-center leading-loose">
+            最新情報をお届けします。
+          </p>
+          <NewsCardList news={news.contents} />
+        </div>
+      </section>
+    </main>
   );
 }
---
+++ components/AnimationSection/index.tsx
@@ -0,0 +1,190 @@
+"use client";
+
+import Image from "next/image";
+import React, { useRef } from "react";
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+import { CSSRulePlugin } from "gsap/CSSRulePlugin";
+import { useGSAP } from "@gsap/react";
+
+gsap.registerPlugin(ScrollTrigger, CSSRulePlugin);
+
+const AnimationSection = () => {
+  const container = useRef<HTMLDivElement>(null);
+  useGSAP(
+    () => {
+      const mm = gsap.matchMedia();
+      // PC用
+      mm.add("(min-width:768px)", () => {
+        const animItems = gsap.utils.toArray<HTMLElement>(".animation-item");
+        animItems.forEach((item) => {
+          const tl = gsap.timeline({
+            scrollTrigger: {
+              trigger: item,
+              start: "top 50%",
+              toggleActions: "play none none",
+            },
+          });
+          tl.from(
+            item.querySelector(".animation-title"),
+            {
+              opacity: 0,
+              y: 40,
+              duration: 0.8,
+            },
+            0
+          );
+          tl.to(
+            item.querySelector(".animation-image__overlay"),
+            {
+              xPercent: 100,
+              duration: 0.8,
+            },
+            0
+          );
+          tl.from(item.querySelector(".animation-text"), {
+            opacity: 0,
+            y: 30,
+            duration: 0.5,
+          });
+        });
+        return () => ScrollTrigger.getAll().forEach((st) => st.kill());
+      });
+      //   スマホ用
+      mm.add("(max-width:767px)", () => {
+        const animItems = gsap.utils.toArray<HTMLElement>(".animation-item");
+        animItems.forEach((item) => {
+          const tl = gsap.timeline({
+            scrollTrigger: {
+              trigger: item,
+              start: "top 50%",
+              toggleActions: "play none none",
+            },
+          });
+          tl.from(item.querySelector(".animation-title"), {
+            opacity: 0,
+            y: 30,
+            duration: 0.5,
+          });
+          tl.from(item.querySelector(".animation-text"), {
+            opacity: 0,
+            y: 20,
+            duration: 0.5,
+          });
+          tl.to(item.querySelector(".animation-image__overlay"), {
+            xPercent: 100,
+            duration: 0.5,
+          });
+        });
+        return () => ScrollTrigger.getAll().forEach((st) => st.kill());
+      });
+      return () => mm.revert();
+    },
+    { scope: container }
+  );
+
+  return (
+    <section className="animation-item py-16 md:py-24 lg:py-32 bg-gray-50">
+      <hgroup className="title mb-10 md:mb-16 lg:mb-20">
+        <h2>セクションタイトルが入ります</h2>
+        <p>Scroll Trigger×TimeLine</p>
+      </hgroup>
+      <div ref={container}>
+        <section className="animation-item w-[90%] mx-auto mb-10 md:mb-12 lg:mb-14">
+          <div className="flex flex-wrap items-center flex-col md:flex-row md:justify-between">
+            <div className="w-full md:w-[48%]">
+              <h2 className="animation-title font-bold text-xl md:text-2xl lg:text-3xl mb-6 md:mb-7 lg:mb-8">
+                タイトル01
+              </h2>
+              <div className="animation-text">
+                <p className="leading-loose">
+                  テキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキスト
+                </p>
+                <p className="leading-loose">
+                  テキストテキストテキストテキストテキストテキスト
+                </p>
+                <p className="leading-loose">テキストテキストテキスト</p>
+              </div>
+            </div>
+            <figure className="animation-image w-full md:w-[50%] mt-6 md:mt-0 aspect-[4-3] relative overflow-hidden">
+              <div
+                className="animation-image__overlay 
+              bg-white w-full h-full absolute left-0 top-0 translate-0"
+                aria-hidden="true"
+              ></div>
+              <Image
+                src="/images/forest.jpg"
+                width={5997}
+                height={4000}
+                alt="森"
+                className="object-cover w-full rounded-3xl"
+              />
+            </figure>
+          </div>
+        </section>
+        <section className="animation-item w-[90%] mx-auto mb-10 md:mb-12 lg:mb-14">
+          <div className="flex flex-wrap items-center flex-col md:flex-row md:justify-between">
+            <div className="w-full md:w-[48%]">
+              <h2 className="animation-title font-bold text-xl md:text-2xl lg:text-3xl mb-6 md:mb-7 lg:mb-8">
+                タイトル01
+              </h2>
+              <div className="animation-text">
+                <p className="leading-loose">テキストテキストテキスト</p>
+                <p className="leading-loose">テキストテキストテキスト</p>
+                <p className="leading-loose">テキストテキストテキスト</p>
+              </div>
+            </div>
+            <figure className="animation-image w-full md:w-[50%] mt-6 md:mt-0 aspect-[4-3] relative overflow-hidden">
+              <div
+                className="animation-image__overlay 
+              bg-white w-full h-full absolute left-0 top-0 translate-0"
+                aria-hidden="true"
+              ></div>
+              <Image
+                src="/images/park.jpg"
+                width={6144}
+                height={4096}
+                alt="公園"
+                className="object-cover w-full rounded-3xl"
+              />
+            </figure>
+          </div>
+        </section>
+        <section className="animation-item w-[90%] mx-auto">
+          <div className="flex flex-wrap items-center flex-col md:flex-row md:justify-between">
+            <div className="w-full md:w-[48%]">
+              <h2 className="animation-title font-bold text-xl md:text-2xl lg:text-3xl mb-6 md:mb-7 lg:mb-8">
+                タイトル01
+              </h2>
+              <div className="animation-text">
+                <p className="leading-loose">テキストテキストテキスト</p>
+                <p className="leading-loose">
+                  テキストテキストテキストテキストテキスト
+                </p>
+                <p className="leading-loose">
+                  テキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキスト
+                </p>
+              </div>
+            </div>
+            <figure className="animation-image w-full md:w-[50%] mt-6 md:mt-0 aspect-[4-3] relative overflow-hidden">
+              <div
+                className="animation-image__overlay 
+              bg-white w-full h-full absolute left-0 top-0 translate-0"
+                aria-hidden="true"
+              ></div>
+              <Image
+                src="/images/river.jpg"
+                width={5760}
+                height={3840}
+                alt="川"
+                className="object-cover w-full rounded-3xl"
+              />
+            </figure>
+          </div>
+        </section>
+      </div>
+    </section>
+  );
+};
+
+export default AnimationSection;
