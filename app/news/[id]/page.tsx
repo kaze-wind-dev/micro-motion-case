84b6160e44d5666ae16e7d0b80685deb4628ad69
HayateIwamoto <iwamoto.hayate.613@gmail.com>
2025-07-06 15:25:10 +0900

    feature: お知らせ詳細ページの追加

---
+++ app/news/[id]/page.tsx
@@ -0,0 +1,41 @@
+import { getArticleList } from "@/libs/microcmsFunctions";
+import { notFound } from "next/navigation";
+import { dateFormat } from "@/libs/utility";
+
+type Props = {
+  params: {
+    id: string;
+  };
+};
+
+export default async function Page({ params }: Props) {
+  const { id } = await params;
+  // APIから記事一覧を取得
+  let data;
+  try {
+    data = await getArticleList({
+      endpoint: `news/${id}`, // 取得したいエンドポイント名を指定
+    });
+  } catch {
+    notFound();
+  }
+  console.log(data);
+  return (
+    <>
+      <article className="py-16 md:py-24 lg:py-32">
+        <div className="inner">
+          <div className="prose-sm md:prose-base lg:prose-lg">
+            <h1>{data.title}</h1>
+            <p className="text-sm lg:text-md text-right text-gray-500 mb-2 lg:mb-3">
+              <time dateTime={data.publishedAt ?? data.createdAt}>
+                {dateFormat(data.publishedAt ?? data.createdAt, ".")}
+              </time>
+            </p>
+            <div dangerouslySetInnerHTML={{ __html: data.content }} />
+          </div>
+          <a href="/news/" className="bg-green-600 hover:bg-green-700 transition text-white text-center py-4 px-6 mx-auto mt-10 block w-fit">一覧に戻る</a>
+        </div>
+      </article>
+    </>
+  );
+}
